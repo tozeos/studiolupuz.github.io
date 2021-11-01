40878102b261f332c68a1cb92c37dc0ab5821065
Caike <caike_tb@ymail.com>
2021-11-01 15:25:34 -0300

    New team section

--- components/Team/index.js
+++ components/Team/index.js
@@ -3,77 +3,38 @@ import styles from "../../styles/team.module.scss";
 import Link from "next/link";
 import Image from "next/image";
 import {team, defaultpfp} from "../variables";
-
-import {Swiper, SwiperSlide} from "swiper/react";
-
-// Import Swiper styles
-import "swiper/swiper.min.css";
-import "swiper/components/navigation/navigation.min.css";
-
-// import Swiper core and required modules
-import SwiperCore, {Autoplay, Navigation} from "swiper/core";
-
-// install Swiper modules
-SwiperCore.use([Autoplay, Navigation]);
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faTwitter} from "@fortawesome/free-brands-svg-icons";
 
 const Team = () => {
     return (
         <>
             <section className={styles.container}>
                 <h2 id={'time'} className={styles.subTitle}>Nosso time</h2>
-                <div className={styles.swipercontainer}>
-                    <Swiper
-                        slidesPerView={3}
-                        spaceBetween={0}
-                        navigation={true}
-                        grabCursor={true}
-                        breakpoints={{
-                            "@0.00": {
-                                slidesPerView: 1,
-                                spaceBetween: 10,
-                            },
-                            "@0.75": {
-                                slidesPerView: 2,
-                                spaceBetween: 20,
-                            },
-                            "@1.00": {
-                                slidesPerView: 3,
-                                spaceBetween: 40,
-                            },
-                            "@1.50": {
-                                slidesPerView: 4,
-                                spaceBetween: 0,
-                            },
-                        }}
-                        className={styles.mySwiper}>
+                <div className={styles.teamContainer}>
                         {team.map((member) => (
-                            <SwiperSlide key={member.id}>
+                            <section key={member.id}>
                                 <div className={styles.member}>
                                     <div
                                         className={styles.profile}
                                         style={{
                                             backgroundImage: `url(${
-                                                member.picture == "" ? defaultpfp : member.picture
+                                                member.picture === "" ? defaultpfp : member.picture
                                             })`,
                                         }}
                                     />
-                                    <p className={styles.text}>{member.name}</p>
-                                    <p className={styles.occupation}>{member.occupation}</p>
-                                    <Link href={member.link} passHref>
+                                    <div className={styles.memberInfo}>
+                                        <p className={styles.text}>{member.name}</p>
+                                        <p className={styles.occupation}>{member.occupation}</p>
+                                        <Link href={member.link} passHref>
                                         <a target="_blank" rel="noopener noreferrer nofollow">
-                                            <Image
-                                                className={styles.socialIcon}
-                                                width={25}
-                                                height={25}
-                                                src={member.socialIcon}
-                                                alt={`Link para o ${member.socialName}`}
-                                            />
+                                            <FontAwesomeIcon className={styles.socialIcon} icon={faTwitter}/>
                                         </a>
                                     </Link>
+                                    </div>
                                 </div>
-                            </SwiperSlide>
+                            </section>
                         ))}
-                    </Swiper>
                 </div>
             </section>
         </>
